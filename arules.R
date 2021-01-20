708ced28cd6881d21d9deabdd4485abba3d0c26d
julian <juliansxpro@gmail.com>
2021-01-19 19:18:23 -0500

    Second

--- arules.R
+++ arules.R
@@ -1,5 +1,3 @@
-setwd('C:/Users/julia/OneDrive/Escritorio/DS/Rpubs/AssociationMining')
-
 # The Online Retail II data set contains all the transactions occurring for 
 # a UK-based and registered, non-store online retail between 01/12/2009 
 # and 09/12/2011.The company mainly sells unique all-occasion gift-ware. 
